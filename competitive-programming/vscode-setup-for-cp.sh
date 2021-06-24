6e80b69e71d7dd39b74a016759e11b3d71e2369e
Shihab Mahamud <shihab4t@gmail.com>
2021-06-24 18:00:14 +0600

    update

--- competitive-programming/vscode-setup-for-cp.sh
+++ competitive-programming/vscode-setup-for-cp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# sudo apt update
-# sudo apt install python3 python3-pip gcc g++
-code --install-extension ms-vscode.cpptools
+sudo apt update
+sudo apt install python3 python3-pip gcc g++
 code --install-extension GitHub.github-vscode-theme
 code --install-extension rafapaulin.try-material-icon-theme
 code --install-extension formulahendry.code-runner
 code --install-extension DivyanshuAgrawal.competitive-programming-helper
+code --install-extension ms-vscode.cpptools
---
+++ install-packages/ungoogled-chromium.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo 'deb http://download.opensuse.org/repositories/home:/ungoogled_chromium/Ubuntu_Focal/ /' | sudo tee /etc/apt/sources.list.d/home-ungoogled_chromium.list > /dev/null
+curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Ubuntu_Focal/Release.key' | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home-ungoogled_chromium.gpg > /dev/null
+sudo apt update
+sudo apt install -y ungoogled-chromium
+
