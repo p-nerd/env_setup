92d91dde048cd82de1991c88140d961b01d734f4
Shihab Mahamud <shihab4t@gmail.com>
2021-08-19 13:31:58 +0600

    remove the && character for the chrome-deb.sh vscode-deb.sh file

--- install-packages/chrome-deb.sh
+++ install-packages/chrome-deb.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
-&& sudo dpkg -i google-chrome-stable_current_amd64.deb \
-&& sudo apt update \
-&& sudo apt --fix-broken install
+#! /bin/bash
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt update
+sudo install -f
