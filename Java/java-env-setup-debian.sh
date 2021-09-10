f904732b27c3e8be1b7beff4bb0d542426950417
Shihab Mahamud <shihab4t@gmail.com>
2021-09-10 17:03:33 +0600

    add some java script

--- Java/java-env-setup-debian.sh
+++ Java/java-env-setup-debian.sh
@@ -3,6 +3,18 @@
 # sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/$1/bin/javac 1
 # sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/$1/bin/java 1
 
+if [[ -f /usr/bin/java ]] 
+then
+    echo "$ sudo rm -v /usr/bin/java"
+    sudo rm -v /usr/bin/java
+    echo
+fi
+if [[ -f /usr/bin/javac ]]
+then
+    echo "$ sudo rm -v /usr/bin/javac"
+    sudo rm -v /usr/bin/javac
+    echo
+fi
 echo "$ sudo ln -vs /usr/lib/jvm/$1/bin/java /usr/bin/java"
 sudo ln -vs /usr/lib/jvm/$1/bin/java /usr/bin/java
 echo
