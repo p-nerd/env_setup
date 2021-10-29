bf412cf855e98e6469bac6e54ed4975d6481b231
Shihab Mahamud <shihab4t@gmail.com>
2021-10-30 02:27:02 +0600

    renamed

--- scripts/zsh-setup.sh
+++ scripts/zsh-setup.sh
@@ -16,6 +16,6 @@ git clone https://github.com/romkatv/powerlevel10k.git /home/$USER/.oh-my-zsh/cu
 git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/$USER/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$USER/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-curl -fLo ~/.zshrc https://raw.githubusercontent.com/shihab4t/EnvSetup/main/dotfile/.zshrc
-curl -fLo ~/.p10k.zsh https://raw.githubusercontent.com/shihab4t/EnvSetup/main/dotfile/.p10k.zsh
+curl -fLo ~/.zshrc https://raw.githubusercontent.com/shihab4t/EnvSetup/main/dotfiles/.zshrc
+curl -fLo ~/.p10k.zsh https://raw.githubusercontent.com/shihab4t/EnvSetup/main/dotfiles/.p10k.zsh
 chsh -s /bin/zsh
