fcbc9148e8e709f8ca24d794807a62bb3e7c4e95
Shihab Mahamud <67628903+shihab4t@users.noreply.github.com>
2021-11-10 10:11:21 +0600

    Update vscode-setup-for-cp.sh

--- scripts/setup-scripts/vscode-setup-for-cp.sh
+++ scripts/setup-scripts/vscode-setup-for-cp.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install python3 python3-pip gcc g++ -y
 code --install-extension GitHub.github-vscode-theme
-code --install-extension rafapaulin.try-material-icon-theme
+code --install-extension PKief.material-icon-theme
 code --install-extension formulahendry.code-runner
 code --install-extension DivyanshuAgrawal.competitive-programming-helper
 code --install-extension ms-vscode.cpptools
