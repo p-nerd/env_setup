06e2c57d5ee60df599535814828102a325a51cc3
Shihab Mahamud <shihab4t@gmail.com>
2022-05-08 17:44:38 +0600

    Updates

---
+++ config/git.config.sh
@@ -0,0 +1,5 @@
+git config --global init.defaultBranch main
+git config --global user.email "shihab4t@gmail.com"
+git config --global user.name "Shihab Mahamud"
+git config --global core.editor "vim"
+
--- vscode-extension-ccpp.sh
+++ vscode-extension-ccpp.sh
@@ -1,7 +1,6 @@
-# code --install-extension GitHub.github-vscode-theme
 code --install-extension dracula-theme.theme-dracula
 code --install-extension PKief.material-icon-theme
 code --install-extension formulahendry.code-runner
 code --install-extension DivyanshuAgrawal.competitive-programming-helper
 code --install-extension ms-vscode.cpptools
-# code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension streetsidesoftware.code-spell-checker
