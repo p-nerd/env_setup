50a43196ad820518dbe7a7b2bbfaab8126e07aa2
Shihab Mahamud <shihab4t@gmail.com>
2022-06-22 15:14:18 +0600

    Updates

--- install/gvm-install.sh
+++ install/gvm-install.sh
@@ -1,5 +1,7 @@
 curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer > script.deb
+chmod +x script.deb
 zsh script.deb
 sudo apt install bison -y
+source /home/shihab4t/.gvm/scripts/gvm
 gvm install go1.18.3 -B
 gvm use go1.18.3 --default
